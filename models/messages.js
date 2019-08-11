32f35262a4b960eed349a9c2f8835f1e9c178a5f
Matt D <dwinell.matt@gmail.com>
2019-08-11 14:50:15 -0400

    added seeds to bottom of models

--- models/messages.js
+++ models/messages.js
@@ -23,6 +23,22 @@ const Messages = sequelize.define('Messages', {
         freezeTableName: true
     });
 
-Messages.sync();
+Messages.sync().then(()=>{
+Messages.create({
+    userInit: 1,
+    userReceiving: 2,
+    message: "hi bob how are you?"
+})
+Messages.create({
+    userInit: 2,
+    userReceiving: 1,
+    message: "pretty good, how do I eat a potato?"
+})
+Messages.create({
+    userInit: 1,
+    userReceiving: 3,
+    message: "Bob is a bit weird."
+})
+});
 
 module.exports = Messages;
