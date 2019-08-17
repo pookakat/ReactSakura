df5804dc703ae4138a2db7c35cdbf64036306a2c
Matt D <dwinell.matt@gmail.com>
2019-08-17 13:24:48 -0400

    hitting button for load matches makes api call of all users

--- sakura/src/components/matchWrapper/matchWrapper.js
+++ sakura/src/components/matchWrapper/matchWrapper.js
@@ -1,38 +1,51 @@
 import React from "react";
 import "./style.css";
 import axios from 'axios';
-import matchCard from '../matchCard/matchCard';
+import MatchCard from '../matchCard/matchCard';
 
 export default class MatchWrapper extends React.Component {
-matchCall = ()=>{
-    // event.preventDefault();
+    state = {
+        matches: []
+    }
+matchCall = (event)=>{
+    event.preventDefault();
+    console.log('match call function activated');
     let route;
     if(window.location.href.includes('local')){
-         route = "http://localHost:3001/api/search"
+         route = "http://localHost:3001/api/all"
     } else {
-       route = "/api/search";
+       route = "/api/all";
     }
-    axios.get(route)
-    .then((res)=>{
-        console.log(res);
-        this.state = {
-            matches: res
-        }
+    console.log(route);
+
+
+    axios.get(route).then(function(res){
+        console.log(res.data);
+        // this.setState({
+        //     matches: res.data
+        // })
+   
+        
+
+    //    res.data.map(match=>(
+    //         <MatchCard key= {match.id} id={match.id} userName={match.userName} distance = {'unknown distance'} gender = {match.gender} summary = {match.intro}></MatchCard>
+    //     ))
+
+    }).catch(function(err){
+        console.log(err);
     })
 
 }
 render(){
     return(
-        <div className = 'cardWrapper' onLoad = {this.matchCall}>
-            <div>
-                {state.matches.map(match=>(
+        <div className = 'cardWrapper'>
+            <button className='match-load' onClick= {this.matchCall}> load potential matches </button>
+            <div id="card-root"></div>
+                {/* {this.state.matches.map(match=>(
                     <MatchCard key= {match.id} id={match.id} userName={match.userName} distance = {'unknown distance'} gender = {match.gender} summary = {match.intro}></MatchCard>
 
-                ))}
+                ))} */}
             </div>
-
-
-        </div>
     )
 }
 
